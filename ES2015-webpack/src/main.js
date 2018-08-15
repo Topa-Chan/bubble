a0f8f8e8048f959fe934846926e1eb1039eaf143
bobcortez <bob.men.cor@gmail.com>
2018-08-14 23:49:46 -0600

    Google Maps Working
    Made Google Maps work on Location page, as well as use Moduling practices.

--- ES2015-webpack/src/main.js
+++ ES2015-webpack/src/main.js
@@ -1,29 +1,5 @@
-function initMap() {
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var slc = new google.maps.LatLng(40.7608, -111.8910);
-    var current = null; //Get Location Variable
-    var qkly = new google.maps.LatLng(40.695170, -111.975158);
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: slc
-    });
-    directionsDisplay.setMap(map);
+import{ initMap, handleLocationError, calculateAndDisplayRoute } from "./map_module";
 
-    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        var start = document.getElementById('start').value;
-        var request = {
-            origin: start,
-            destination: qkly,
-            travelMode: 'DRIVING'
-        };
-        directionsService.route(request, function(result, status){
-            if (status == 'OK') {
-                directionsDisplay.setDirections(response);
-            } else {
-                window.alert('Directions request failed due to ' + status);
-            }
-        });
-    }
+module.exports = {
+    initMap, handleLocationError, calculateAndDisplayRoute
 }
-
